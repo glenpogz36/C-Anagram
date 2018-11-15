15ab917ac91e3c9aca9f64f04d2cce0ee1cf5c2a
Glen Sale and James Cho <james@>
2018-11-15 15:39:34 -0800

    unfinish

--- Program.cs
+++ Program.cs
@@ -1,37 +1,79 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     
     public static void Main()
     {
+        List<string> wordList = new List<string>();
+        List<string> anagramList = new List<string> {};
+        string wordEntered;
+       
+    
 
-        Console.WriteLine("Enter a word to convert into Anagram");
-        string wordEntered = Console.ReadLine();
-        char[] arr;
-        arr = wordEntered.ToCharArray();
-       Array.Sort(arr);
+        Console.WriteLine("Enter a word to check Anagram");
+       string userInput = Console.ReadLine();
+
+       Console.WriteLine("Enter another word to compare");
+       wordEntered = Console.ReadLine();
+        
+        while(!string.IsNullOrEmpty(wordEntered) && wordEntered!="Q")
+        {   
+        
+       wordList.Add(wordEntered);
         
 
-         Console.WriteLine("Enter a new word to compare");
+         Console.WriteLine("Enter a new word to compare or 'Q' to Quit");
          wordEntered = Console.ReadLine();
-        char[] arr2;
-        arr2 = wordEntered.ToCharArray();
-       Array.Sort(arr2);
-      
+      }
 
-        string firstWord = new string (arr);
-        string secondWord = new string (arr2);
 
-        if (firstWord == secondWord)
-        {
-            Console.WriteLine("The word is Anagram");
-        }
-        else 
-        {
-            Console.WriteLine("It's not a Anagram");
-        }
 
+   
+   
+   CheckAnagram myAnagram = new CheckAnagram();
+            foreach (string listString in wordList)
+            {
+                if (myAnagram.IsAnagram(userInput, listString))
+                {
+                anagramList.Add(listString);
+                Console.WriteLine("We've found an anagram in your list!");
+               
+                }
+               
+            }     
     }
+
+   
+
+    public class CheckAnagram
+    {
+        public bool IsAnagram(string Input, string fromList)
+        {
+            char[] arrayOne = Input.ToCharArray();
+            char[] arrayTwo = fromList.ToCharArray();
+
+            Array.Sort(arrayOne);
+            Array.Sort(arrayTwo);
+
+            return arrayOne.SequenceEqual(arrayTwo);
+        }
+    }
+
+        // string firstWord = new string (arr);
+        // string secondWord = new string (arr2);
+
+        // if (firstWord == secondWord)
+        // {
+        //     Console.WriteLine("The word is Anagram");
+        // }
+        // else 
+        // {
+        //     Console.WriteLine("It's not a Anagram");
+        // }
+
+   
+      
 }
